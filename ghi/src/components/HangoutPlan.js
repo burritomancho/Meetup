3e11371ed69d197d43c5a9a8a65fa81e2b02c8f6
Robin Kim <robinkim55@gmail.com>
2023-06-07 21:30:06 -0700

    Some stuf

--- ghi/src/components/HangoutPlan.js
+++ ghi/src/components/HangoutPlan.js
@@ -1,16 +1,59 @@
 import { Link } from "react-router-dom";
-import QRCode from "../assets/QRlocalhost3k.png";
+import { useState } from "react";
 import { BsArrowUpRightSquare } from "react-icons/bs";
+import emailjs from "emailjs-com";
 
 export default function HangoutPlan() {
+  const [recipientEmail, setRecipientEmail] = useState("");
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .send(
+        "service_6sd6o2c",
+        "template_l1o0rqa",
+        {
+          from_name: "notification.meetup@gmail.com",
+          message: "Email content",
+          to_email: recipientEmail,
+        },
+        "YOUR_USER_ID"
+      )
+      .then((response) => {
+        console.log("Email sent successfully!", response.status, response.text);
+      })
+      .catch((error) => {
+        console.error("Error sending email:", error);
+      });
+  };
+
+  const handleRecipientEmailChange = (e) => {
+    setRecipientEmail(e.target.value);
+  };
+
   return (
     <div className="pl-16">
       <h3 className="text-center pt-20">Plan a hangout!</h3>
       <div className="pl-10 min-h-screen text-center py-16 grid lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-2">
         <div className="rounded-xl p-2 border border-black-2 lg:h-1/3 md:h-1/3 sm:h-1/2 h-3/4 w-[90%]">
           <div className="p-2">Who are you hanging out with?</div>
-          <div className="p-6 border border-black-2 rounded-xl">
-            Share this link: <img src={QRCode} alt="qr-code" />
+          <div className="p-6 border border-black-2 rounded-xl text-center grid-row-2 grid">
+            <form onSubmit={sendEmail}>
+              <input
+                type="email"
+                placeholder="Recipient Email"
+                value={recipientEmail}
+                onChange={handleRecipientEmailChange}
+                required
+                className="text-center"
+              />
+            </form>
+            <button
+              type="submit"
+              className="text-center border-black border mx-48"
+            >
+              Send Email
+            </button>
           </div>
         </div>
         <div className="rounded-xl p-2 border border-black-2 lg:h-1/3 md:h-1/3 sm:h-1/2 h-3/4 w-[90%]">
